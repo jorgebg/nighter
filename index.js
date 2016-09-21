2693e33d1c9cdf04e2f0a15309ac192e400b132f
Jorge Barata <jorge.barata@udemy.com>
2016-09-21 14:09:06 +0100

    add input on index

--- index.js
+++ index.js
@@ -7,30 +7,48 @@ var app = express();
 app.set('port', (process.env.PORT || 8000));
 
 app.get('/', function(req, res) {
-    request(req.query.url, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-          var $ = cheerio.load(body)
-          var style = `
-              <style>
-              /* https://lnikki.la/articles/night-mode-css-filter/ */
-              html, img, video {
-                  -webkit-filter: invert(1) hue-rotate(180deg);
-                  filter: invert(1) hue-rotate(180deg);
-              }
+    if (!req.query.url) {
+        var index = `
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>Nighter</title>
+        </head>
+        <body>
+        <form>
+        <input name="url"></input>
+        </form>
+        </body>
+        </html>
+        `;
+        res.send(index);
+    } else {
+        request(req.query.url, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var $ = cheerio.load(body)
+                var style = `
+                  <style>
+                  /* https://lnikki.la/articles/night-mode-css-filter/ */
+                  html, img, video {
+                      -webkit-filter: invert(1) hue-rotate(180deg);
+                      filter: invert(1) hue-rotate(180deg);
+                  }
 
-              body {
-                  background: black;
-              }
-              </style>
-              `;
+                  body {
+                      background: black;
+                  }
+                  </style>
+                  `;
 
-          $('head').append(style);
-          res.send($.html());
-      }
-    })
+                $('head').append(style);
+                res.send($.html());
+            }
+        })
+    }
 });
 
 
-app.listen(app.get('port'), function () {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
 });

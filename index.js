55845249b4e0eae46fa2f1e14d48088407c60041
Jorge Barata <jorge.barata@udemy.com>
2016-09-21 13:34:00 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var request = require('request');
+var express = require('express');
+var cheerio = require('cheerio');
+
+
+var app = express();
+app.set('port', (process.env.PORT || 8000));
+
+app.get('/', function(req, res) {
+    request(req.query.url, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+          var $ = cheerio.load(body)
+          var style = `
+              <style>
+              /* https://lnikki.la/articles/night-mode-css-filter/ */
+              html, img, video {
+                  -webkit-filter: invert(1) hue-rotate(180deg);
+                  filter: invert(1) hue-rotate(180deg);
+              }
+
+              body {
+                  background: black;
+              }
+              </style>
+              `;
+
+          $('head').append(style);
+          res.send($.html());
+      }
+    })
+});
+
+
+app.listen(app.get('port'), function () {
+  console.log('Node app is running on port', app.get('port'));
+});
